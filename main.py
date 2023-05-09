e3aee9fc13fb59a33cb011b998623d7de2485d27
Gabriel Kocenko <gabryelkocenko@gmail.com>
2023-05-09 16:56:43 +0200

    Added converting bytes to int

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from png import png_image
 
 
-input_file = "imgs/baboon.png"
-
 if __name__ == '__main__':
+    input_file = "imgs/baboon.png"
     image = png_image.PNG(input_file)
     image.find_chunks()
--- png/png_image.py
+++ png/png_image.py
@@ -8,6 +8,7 @@ class PNG:
     def __init__(self, file_name: str):
         self.__file_name = file_name
         self.__data = self.__read_file()
+        self.__chunks = self.__init_chunks()
         
         if not self.__is_png():
             raise ValueError("Given file is not a PNG file or it is corrupted")
@@ -24,13 +25,26 @@ class PNG:
 
         return bytes
 
+    def __init_chunks(self) -> dict:
+        ''' Creates chunks dict and initializes with None
+
+        Returns:
+            Dict with chunk names as keys and None as their values
+        '''
+
+        chunks = {}
+        for chunk_name in self.list_of_chunks:
+            chunks[chunk_name] = None
+
+        return chunks 
+
     def __is_png(self) -> bool:
         ''' Checks if the file begins with the PNG signature
 
         Returns:
             Returns True if its begins with the PNG signature, else False
         '''
-        
+
         return True if self.__data[:8] == self.signature else False
     
     @staticmethod
@@ -47,8 +61,25 @@ class PNG:
         bytes_as_string = "".join(map(chr, bytes))
         return bytes_as_string
     
+    @staticmethod
+    def __bytes_to_int(bytes: bytearray) -> int:
+        """ Converts bytearray to int
+
+        Args:
+            bytes:
+                bytearray to convert
+        Returns:
+            Converted int
+        """
+        return int.from_bytes(bytes, 'big', signed=False)
+
+    def __read_chunks(self) -> None:
+        print(self.__bytes_to_int(self.__data[8:12]))
+
     def find_chunks(self):
-        for i in range(len(self.__data) - 4):
-            current_string = self.__bytes_to_string(self.__data[i:i+4])
-            if current_string in self.list_of_chunks:
-                print(f'Found {current_string} chunk')
+        self.__read_chunks()
+    #     for i in range(len(self.__data) - 4):
+    #         current_string = self.__bytes_to_string(self.__data[i:i+4])
+    #         if current_string in self.list_of_chunks:
+    #             print(f'Found {current_string} chunk')
+
